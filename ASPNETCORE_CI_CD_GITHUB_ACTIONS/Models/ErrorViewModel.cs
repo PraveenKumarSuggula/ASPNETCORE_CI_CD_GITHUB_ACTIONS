d596752794b4394065c9ecae44562ac8b9f7490c
PraveenSuggula <you@example.compraveenkumar.suggula0@gmail.com>
2024-03-03 00:04:25 -0600

    Add project files.

---
+++ ASPNETCORE_CI_CD_GITHUB_ACTIONS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETCORE_CI_CD_GITHUB_ACTIONS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
